eaa401b6b712cb81f22963d24452e785f2dcfb82
OCEANOFANYTHING <VIRUSSERVER8697@GMAIL.COM>
2025-04-04 13:25:02 -0700

    update(error fix): client side error fixes

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
